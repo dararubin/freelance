b42bc326fbe5f1cfd567aa60a89635eb9d534818
dararubin <dararubin2017@u.northwestern.edu>
2016-11-29 18:38:08 -0600

    whole site I hope

---
+++ modals2.js
@@ -0,0 +1,28 @@
+window.onload = function(){ 
+
+    // Get the modal
+    var modal99 = document.getElementById('myModal99');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn99");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close99")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+	modal99.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+	modal99.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+	if (event.target == modal99) {
+		modal99.style.display = "none";
+	}
+}
+};
---
+++ st.js
@@ -0,0 +1,63 @@
+window.onload = function(){ 
+
+
+$(document).ready(function() {
+
+			$('#show1').click(function(){
+
+				$('#coachscenepartner').slideToggle( );  
+
+
+			});
+
+			$('#show2').click(function(){
+
+				$(' #coachscenepartner').slideToggle();
+
+
+
+			});
+
+
+			$('#show3').click(function(){
+
+				$('#inperson').slideToggle();   
+
+			});
+
+			$('#show4').click(function(){
+
+				$('#virtual').slideToggle();   
+
+			});
+
+			$('#show5').click(function(){
+
+				$('#classlocation').slideToggle();   
+
+			});
+
+			$('#show6').click(function(){
+
+				$('#skills').slideToggle();   
+
+			});
+
+			$('#show7').click(function(){
+
+				$('#time').slideToggle();   
+
+			});
+			$('#show8').click(function(){
+
+				$('#duration').slideToggle();   
+
+			});
+
+			$('#show9').click(function(){
+
+				$('#purchase').slideToggle();   
+
+			});
+		});
+};
